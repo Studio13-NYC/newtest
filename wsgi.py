0462b7df1f2b0870b9239e7a057bd6a85476cd8c
nickknyc <nick@katsivelos.com>
2025-01-05 12:31:04 -0500

    Cleanup: Remove unnecessary files and update configuration

--- wsgi.py
+++ wsgi.py
@@ -1,12 +1,4 @@
 from app.main import app
-from fastapi.middleware.wsgi import WSGIMiddleware
-import uvicorn
-
-# Create WSGI app
-wsgi_app = WSGIMiddleware(app)
 
 # This is needed for Azure App Service
-application = wsgi_app
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+application = app
